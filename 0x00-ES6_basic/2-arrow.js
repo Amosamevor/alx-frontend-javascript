885f01a6b1cdf630ccf00977f80585430b50d094
Amosamevor <104185979+Amosamevor@users.noreply.github.com>
2024-01-03 18:01:36 +0000

    3. Parameter defaults

---
+++ 0x00-ES6_basic/2-arrow.js
@@ -0,0 +1,14 @@
+export default const getNeighborhoodsList = () => {
+  const sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  const addNeighborhood = (newNeighborhood) => {
+    sanFranciscoNeighborhoods.push(newNeighborhood);
+    return sanFranciscoNeighborhoods;
+  };
+
+  return {
+    addNeighborhood
+  };
+};
+
+
